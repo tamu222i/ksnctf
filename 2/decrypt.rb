ed795381a16401a407eac35aa992c46a8cd4221c
masaki-tamura <masaki-tamura@kccs.co.jp>
2018-05-21 13:14:46 +0900

    clear

--- 2/decrypt.rb
+++ 2/decrypt.rb
@@ -1,16 +1,26 @@
 #!/usr/bin/env ruby
 lines = File.read('./text.txt')
-bs = lines.bytes.to_a
+table = {}
+('a'..'z').each do |al|
+  table[al.ord] = (al.ord - 13 < 'a'.ord) ? al.ord - 13 + 26: al.ord - 13
+end
+('A'..'Z').each do |al|
+  table[al.ord] = (al.ord - 13 < 'A'.ord) ? al.ord - 13 + 26: al.ord - 13
+end
+p table
+
 as = []
+bs = lines.bytes.to_a
 bs.each do |b|
-  if b == 32
+  if [32, 46, 10, 44].include?(b)
     as.push(b)
     next
   end
-  as.push(b - 13)
+  as.push(table[b])
 end
 
 p bs
+p as
 print bs.pack('c*')
 print as.pack('c*')
 
